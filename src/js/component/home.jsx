d809fd463895920677fff556c00c8705b4a28acb
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-03-25 17:47:54 +0000

    falta footer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,23 +2,42 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Card from "./card";
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
 
 //create your first component
+let components = [];
+for (let i = 0; i < 4; i++) {
+    components.push(
+		<div key={i} className="col">
+		  <Card />
+		</div>
+	  );
+	}
+
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	
+		<div>
+			<Navbar />
+			<div className ="container">
+				<Jumbotron />
+
+				<div className="row">
+				
+					{components}
+					
+				</div>
+	
+			</div>
+			
+				<div class="card">
+ 				 <div class="card-footer container-fluid d-flex">
+					footer
+				</div>
+				</div>
+
 		</div>
 	);
 };
