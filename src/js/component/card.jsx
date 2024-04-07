0158a70c7ca0ab509157b155eee638ea575072cc
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-04-07 14:56:01 +0000

    eliminado style y terminado

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import reactDom  from 'react-dom'
 const Card = function MyCard(){
     return(
-    <div className="card" style="width: 18rem;">
+    <div className="card">
       <img src="https://i.pinimg.com/564x/e6/c3/4a/e6c34afdf235e76c31344d6a933cae27.jpg" className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title d-flex justify-content-center">Card title</h5>
--- src/js/component/navbarFooter.jsx
+++ src/js/component/navbarFooter.jsx
@@ -3,7 +3,7 @@ import reactDom  from 'react-dom'
 const NavBarFooter = () => {
     
     return(
-        <nav className="navbar fixed-bottom navbar-light bg-dark d-flex justify-content-center p-3">
+        <nav className="navbar mt-4 bottom navbar-light bg-dark d-flex justify-content-center p-3">
             <a className="navbar-brand" href="#">Copyright Annita Dinamita</a>
         </nav>
         )
